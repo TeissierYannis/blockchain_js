7d7e2a560033e10e1d9e16662116774faee72912
Teissier Yannis <yannis.teissier70@gmail.com>
2022-08-19 15:14:47 +0200

    validate transactions

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,9 @@
-const Block = require('./block');
+const Block = require("./block");
+const {REWARD_INPUT, MINING_REWARD} = require("../config/config");
+const Transaction = require("../wallet/transaction");
 const {cryptoHash} = require("../util");
+const Wallet = require("../wallet");
+
 
 class Blockchain {
     constructor() {
@@ -8,38 +12,91 @@ class Blockchain {
 
     addBlock({ data }) {
         const newBlock = Block.mineBlock({
-            lastBlock: this.chain[this.chain.length - 1],
+            lastBlock: this.chain[this.chain.length-1],
             data
         });
 
         this.chain.push(newBlock);
     }
 
-    replaceChain(chain, onSuccess) {
-
-        if(chain.length <= this.chain.length) {
+    replaceChain(chain, validateTransactions, onSuccess) {
+        if (chain.length <= this.chain.length) {
             console.error('The incoming chain must be longer');
             return;
         }
 
-        if(!Blockchain.isValidChain(chain)) {
+        if (!Blockchain.isValidChain(chain)) {
             console.error('The incoming chain must be valid');
             return;
         }
 
+        if (validateTransactions && !this.validTransactionData({ chain })) {
+            console.error('The incoming chain has invalid transaction data');
+            return;
+        }
+
         if (onSuccess) onSuccess();
-        console.log('Replacing chain with', chain);
+        console.log('replacing chain with', chain);
         this.chain = chain;
     }
 
+    validTransactionData({ chain }) {
+        for (let i=1; i<chain.length; i++) {
+            const block = chain[i];
+            const transactionSet = new Set();
+            let rewardTransactionCount = 0;
+
+            for (let transaction of block.data) {
+                if (transaction.input.address === REWARD_INPUT.address) {
+                    rewardTransactionCount += 1;
+
+                    if (rewardTransactionCount > 1) {
+                        console.error('Miner rewards exceed limit');
+                        return false;
+                    }
+
+                    if (Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
+                        console.error('Miner reward amount is invalid');
+                        return false;
+                    }
+                } else {
+                    if (!Transaction.validTransaction(transaction)) {
+                        console.error('invalid transaction');
+                        return false;
+                    }
+
+                    const trueBalance = Wallet.calculateBalance({
+                        chain: this.chain,
+                        address: transaction.input.address
+                    });
+
+                    if (transaction.input.amount !== trueBalance) {
+                        console.error('Invalid inout amount');
+                        return false;
+                    }
+
+                    if (transactionSet.has(transaction)) {
+                        console.error('Identical transaction appears more than once in the block');
+                        return false;
+                    } else {
+                        transactionSet.add(transaction);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
     static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false
+        }
 
-        if (JSON.stringify(chain[0])  !== JSON.stringify(Block.genesis())) return false;
-
-        for (let i = 1; i < chain.length; i++) {
-            const { timestamp, lastHash, hash, data, nonce, difficulty } = chain[i];
-            const actualLastHash = chain[i - 1].hash;
-            const lastDifficulty = chain[i - 1].difficulty;
+        for (let i=1; i<chain.length; i++) {
+            const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
+            const actualLastHash = chain[i-1].hash;
+            const lastDifficulty = chain[i-1].difficulty;
 
             if (lastHash !== actualLastHash) return false;
 
@@ -47,8 +104,9 @@ class Blockchain {
 
             if (hash !== validatedHash) return false;
 
-            if ((lastDifficulty - difficulty) > 1) return false;
+            if (Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
+
         return true;
     }
 }
